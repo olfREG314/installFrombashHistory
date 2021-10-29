1bff96c8e1714c914afc2bda868810c466662023
olfREG314 <olfreg314@gmail.com>
2021-10-29 14:32:17 +0530

    minor changes

--- installingFromBashHistory.sh
+++ installingFromBashHistory.sh
@@ -18,7 +18,7 @@ do
 #finding lines with starting name sudo apt install
 	if [[ "$CURRENT_LINE" =~ "sudo apt install" ]]
 	    then 
-	    echo -e "$LINE: $CURRENT_LINE __do you want to install it (y/n):"
+	    echo -e "$LINE: $CURRENT_LINE __do you want to install it (y/n[any key will work]):"
 		#taking inout from the current terminal or swap with your current terminal using command 'tty'...
 	        read ANS < $CURRENT_TERMINAL #its takes input from user via terminal script is running
 	        case "$ANS" in 
