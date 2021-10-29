d874a3086c6181e8155e7a64be9dfab0126735bc
olfREG314 <olfreg314@gmail.com>
2021-10-29 13:46:29 +0530

    changes make to access every terminal and their instances '

--- installingFromBashHistory.sh
+++ installingFromBashHistory.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
 
-FILE=~/.bash_history
-#FILE=~/shellFile/test.txt
+#location of the file
+FILE=~/.bash_history  
 
-#checking if file exists or not 
-
+echo "\n"
+#check if file exists or not 
 if [ -f "$FILE" ]
-then echo "$FILE exists - start readin  the file"
-else echo "$FILE not exists - check the location of the file || change the location of the file in the script"
+then echo "$FILE exists - start reading the lines"
+else echo "$FILE does not exists - check the location of the file || change the location of the file in the script"
 fi
+
+#getting the current terminal
+CURRENT_TERMINAL=$(tty)
 
 while read CURRENT_LINE
 do
@@ -17,10 +20,10 @@ do
 	    then 
 	    echo -e "$LINE: $CURRENT_LINE __do you want to install it (y/n):"
 		#taking inout from the current terminal or swap with your current terminal using command 'tty'...
-	        read ANS < dev/pts/0
+	        read ANS < $CURRENT_TERMINAL #its takes input from user via terminal script is running
 	        case "$ANS" in 
 	        [y])
-	            $CURRENT_LINE
+	            $CURRENT_LINE #run the command
 	            ;;
 	        *)
 	            echo "going onto next line"
